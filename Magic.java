330020b44225a1f9e67687ffda1884a09d7aa988
puja695 <pujasrk.2005@gmail.com>
2025-01-17 19:02:11 +0530

    Code

---
+++ Magic.java
@@ -0,0 +1,24 @@
+public class Magic {
+public static void main(String[] args) {
+    int myNumber=7;
+    
+    //first step 
+    int stepOne=7*7;
+    
+    //second step
+    int stepTwo=stepOne+myNumber;
+    
+    //third step
+    int stepThree=stepTwo/7;
+    
+    //fourth step
+    int stepFour=stepThree+17;
+    
+    //fifth step
+    int stepFive=stepFour-7;
+    
+    //sixth step
+    int stepSix=stepFive/6;
+    
+    System.out.println(stepSix);
+    }}
